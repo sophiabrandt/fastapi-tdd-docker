7feaf3cc18bbc1d8424f27f53170bdfecead4661
Sophia Brandt <git@s-br.net>
2020-06-29 22:03:53 +0200

    📦 NEW: add get single summary

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -15,3 +15,9 @@ async def post(payload: SummaryPayloadSchema) -> int:
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    return None
