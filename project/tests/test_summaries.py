2df34ba565ab9a1c19a3b16d42c08861eea40fe1
Sophia Brandt <git@s-br.net>
2020-07-02 09:40:37 +0200

    👌 IMPROVE: update put route

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -105,6 +105,22 @@ def test_update_summary_incorrect_id(test_app_with_db):
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
+    response = test_app_with_db.put(
+        "/summaries/0/",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
+    )
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["path", "id"],
+                "msg": "ensure this value is greater than 0",
+                "type": "value_error.number.not_gt",
+                "ctx": {"limit_value": 0},
+            }
+        ]
+    }
+
 
 def test_update_summary_invalid_json(test_app_with_db):
     response = test_app_with_db.post(
@@ -149,6 +165,13 @@ def test_update_summary_invalid_keys(test_app_with_db):
             }
         ]
     }
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
+    )
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
 
 
 def test_create_summaries_invalid_json(test_app):
