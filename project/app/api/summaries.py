c0bfcf8f0f79c3fbd6113dac34cc43ad3367e364
Sophia Brandt <git@s-br.net>
2020-07-02 08:33:30 +0200

    👌 IMPROVE: add extra validation to single get route

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -2,7 +2,7 @@
 
 from typing import List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
 from app.models.tortoise import SummarySchema
@@ -30,7 +30,7 @@ async def read_all_summaries() -> List[SummarySchema]:
 
 
 @router.get("/{id}/", response_model=SummarySchema)
-async def read_summary(id: int) -> SummarySchema:
+async def read_summary(id: int = Path(..., gt=0)) -> SummarySchema:
     summary = await crud.get(id)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
